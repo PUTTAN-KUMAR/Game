ee666f29accdcd20dd0b84402a821237df118bed
puttan kumar <puttankumar76@gmail.com>
2025-04-26 21:14:05 +0530

    add new file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import random
 '''
 a for snake
 -1 for water
@@ -8,5 +10,57 @@ a for snake
 '''
 
 computer = -1
-you =input("Enter your choice: ")
-y
+youstr =input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+
+you = youDict[youstr]
+
+print(f" You chose {reverseDict[you]}\nComputer chose  {reverseDict[computer]}")
+
+if(computer == you ):
+    print("its a draw")
+
+
+else:
+   if(computer == -1 and you == 1):
+    print("You win!")
+
+   elif(computer == -1 and you == 0):
+    print("You lose!")
+
+   elif(computer == 1 and you == -1):
+    print("You lose!")
+
+   elif(computer == 1 and you ==0):
+    print("You win!")
+
+   elif(computer == 0 and you ==-1):
+    print("You win!")
+
+   elif(computer == 0 and you ==1):
+    print("You lose!")
+
+   else:
+    print("Something went wrong!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
